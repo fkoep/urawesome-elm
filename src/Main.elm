aa7485e3be6adeddd11651d12f5e8d23db891cd9
Felix Köpge <fkoep@mailbox.org>
2020-01-26 23:00:20 +0100

    Make Main work with latest Tafl.View changes

--- src/Main.elm
+++ src/Main.elm
@@ -10,23 +10,23 @@ import WebUtil exposing (onEnter)
 import Tafl.Game exposing (Game)
 import Tafl.Game as Game
 import Tafl.Variants as Variants
-import Tafl.View exposing (View)
+import Tafl.View exposing (ViewModel)
 import Tafl.View as View
 
 type Msg
     = CreateSession String
     | LoadSession String
     | DeleteSession String
-    | GameMsg String Game.Action
-    | ViewMsg View.Event
+    | GameAction String Game.Action
+    | ViewEvent View.Event
 
 type alias Model =
-    { active: Maybe (String, View)
+    { active: Maybe (String, ViewModel)
     , sessions: Dict String Game
     }
 
 init =
-    { active = Just ("mygame" , View.create)
+    { active = Just ("mygame" , View.init)
     , sessions = Dict.singleton "mygame" <| Game.create Variants.fetlarBoard
     }
 
@@ -35,35 +35,35 @@ update msg model =
         CreateSession name ->
             { model
             | sessions = Dict.insert name (Game.create Variants.fetlarBoard) model.sessions
-            , active = Just (name, View.create)
+            , active = Just (name, View.init)
             }
         DeleteSession name ->
             { model | sessions = Dict.remove name model.sessions }
         LoadSession name ->
-            { model | active = Just (name, View.create) }
-        GameMsg name gmsg ->
-            { model | sessions = Dict.update name (Maybe.map <| Game.update gmsg) model.sessions }
-        ViewMsg vmsg ->
+            { model | active = Just (name, View.init) }
+        GameAction name act ->
+            { model | sessions = Dict.update name (Maybe.map <| Game.update act) model.sessions }
+        ViewEvent evt ->
             let updateview view2 = { model | active = Maybe.map (Tuple.mapSecond <| always view2) model.active } in
             model.active
             |> Maybe.map (\(name, view) -> 
-                case View.viewUpdate vmsg view of
-                    (Just act, view2) -> update (GameMsg name act) (updateview view2)
+                case View.eventUpdate evt view of
+                    (Just act, view2) -> update (GameAction name act) (updateview view2)
                     (Nothing, view2) -> updateview view2)
             |> Maybe.withDefault model
 
-renderGame: Model -> (String, View) -> Html Msg
-renderGame model (name, view) =
+viewGame: Model -> (String, ViewModel) -> Html Msg
+viewGame model (name, view) =
     div 
         []
         [ h2 [] [text name]
         , Dict.get name model.sessions
-        |> Maybe.map (\game -> Html.map ViewMsg <| View.render game view)
+        |> Maybe.map (\game -> Html.map ViewEvent <| View.view game view)
         |> Maybe.withDefault (div [] [])
         ]
 
-renderSessionCreator: Html Msg
-renderSessionCreator =
+viewSessionCreator: Html Msg
+viewSessionCreator =
     div 
         []
         [input
@@ -74,26 +74,26 @@ renderSessionCreator =
             []
         ]
 
-renderSessionEntry: (String, Game) -> Html Msg
-renderSessionEntry (name, game) =
+viewSessionEntry: (String, Game) -> Html Msg
+viewSessionEntry (name, game) =
     li
         [onClick (LoadSession name)]
         [text name]
 
-renderSessionList: List (String, Game) -> Html Msg
-renderSessionList sessions =
+viewSessionList: List (String, Game) -> Html Msg
+viewSessionList sessions =
     ul
         [class "session-list"]
-        (List.map renderSessionEntry sessions)
+        (List.map viewSessionEntry sessions)
 
 webview model =
     div
         []
         [ model.active
-        |> Maybe.map (renderGame model)
+        |> Maybe.map (viewGame model)
         |> Maybe.withDefault (div [] [])
-        , renderSessionCreator
-        , renderSessionList <| Dict.toList model.sessions
+        , viewSessionCreator
+        , viewSessionList <| Dict.toList model.sessions
         ]
         
 main = Browser.sandbox
